195043ea9403b4204a50c96f9cdfddc8f63ca3bd
Andrew Pham <69804999+andrewp8@users.noreply.github.com>
2024-03-05 22:51:07 +0000

    AP added pundit authorization

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,30 +1,39 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
   before_action :ensure_current_user_is_owner, only: [:destroy, :update, :edit]
+  before_action :ensure_user_is_authorized, only: [:show]
 
   # GET /photos or /photos.json
   def index
     @photos = Photo.all
+    
+    # authorize @photo
   end
 
   # GET /photos/1 or /photos/1.json
   def show
+    authorize @photo
   end
 
   # GET /photos/new
   def new
     @photo = Photo.new
+    
+    authorize @photo
   end
 
   # GET /photos/1/edit
   def edit
+    authorize @photo
   end
 
   # POST /photos or /photos.json
   def create
     @photo = Photo.new(photo_params)
     @photo.owner = current_user
-
+    
+    authorize @photo
+    
     respond_to do |format|
       if @photo.save
         format.html { redirect_to @photo, notice: "Photo was successfully created." }
@@ -38,6 +47,8 @@ class PhotosController < ApplicationController
 
   # PATCH/PUT /photos/1 or /photos/1.json
   def update
+    authorize @photo
+
     respond_to do |format|
       if @photo.update(photo_params)
         format.html { redirect_to @photo, notice: "Photo was successfully updated." }
@@ -51,6 +62,8 @@ class PhotosController < ApplicationController
 
   # DELETE /photos/1 or /photos/1.json
   def destroy
+    authorize @photo
+
     @photo.destroy
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Photo was successfully destroyed." }
@@ -72,6 +85,12 @@ class PhotosController < ApplicationController
     def ensure_current_user_is_owner
       if current_user != @photo.owner
         redirect_back fallback_location: root_url, alert: "You're not authorized for that."
+      end
+    end
+
+    def ensure_user_is_authorized
+      if !PhotoPolicy.new(current_user, @photo).show?
+        raise Pundit::NotAuthorizedError, "not allowed"
       end
     end
 end
